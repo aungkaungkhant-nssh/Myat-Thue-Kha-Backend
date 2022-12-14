8ab30020fe2b89dee90abff0019f14fbec26ecf5
aungkaungkhant-nssh <akkgit0909@gmail.com>
2022-12-14 23:28:45 +0630

    cors origin

--- index.js
+++ index.js
@@ -8,8 +8,12 @@ const uuidv4 = require('uuid');
 const compression = require('compression');
 const cors = require("cors");
 require('dotenv').config();
-app.use(cors());
-
+const corsOptions ={
+    origin:'http://localhost:3000', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200
+}
+app.use(cors(corsOptions));
 
 const fileStorage = multer.diskStorage({
     destination:(req,file,cb)=>{
